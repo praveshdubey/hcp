96acf38dd666bd3ebdff954de05251bf643df3f2
Pravesh Dubey <praveshd97@gmail.com>
2025-12-03 19:54:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ region = "ap-south-1"
 resource "aws_instance" "three" {
   count         = 1
   ami           = "ami-0d176f79571d18a8f"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "dev-server"
