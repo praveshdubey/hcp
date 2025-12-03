f5805a1f3b346b362ff6b04017ed5a0b5279fbf4
Pravesh Dubey <praveshd97@gmail.com>
2025-12-03 19:23:15 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+region = "ap-soouth-1"
+}
+
+resource "aws_instance" "three" {
+  count         = 1
+  ami           = "ami-0d176f79571d18a8f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "dev-server"
+  }
+}
